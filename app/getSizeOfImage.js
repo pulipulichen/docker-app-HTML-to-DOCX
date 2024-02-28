d6c2f4dcc0c13bb36bba39dc9a97a4c34b69a745
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-28 23:08:54 +0800

    西元2024年02月28日 (週三) 23時08分54秒 CST

--- app/getSizeOfImage.js
+++ app/getSizeOfImage.js
@@ -5,16 +5,22 @@ const sizeOf = require('image-size');
 module.exports = function(imageUrl) {
   return new Promise(function(resolve, reject) {
     // Make an HTTP GET request to fetch the image
-    request({ url: imageUrl, encoding: null }, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        // Get image dimensions
-        const dimensions = sizeOf(body);
-        resolve(dimensions)
-        // console.log(`Image width: ${dimensions.width}px`);
-        // console.log(`Image height: ${dimensions.height}px`);
-      } else {
-        reject(error);
-      }
-    });
+    console.log({imageUrl})
+    try {
+      request({ url: imageUrl, encoding: null }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          // Get image dimensions
+          const dimensions = sizeOf(body);
+          resolve(dimensions)
+          // console.log(`Image width: ${dimensions.width}px`);
+          // console.log(`Image height: ${dimensions.height}px`);
+        } else {
+          reject(error);
+        }
+      });
+    }
+    catch (e) {
+      reject(e)
+    }
   })
 }
