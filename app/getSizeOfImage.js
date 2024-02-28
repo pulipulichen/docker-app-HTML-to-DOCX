cc5d1ce1c90a63c8e7f734317b33c311b4dc8dd1
Pulipuli Chen <pulipuli.chen@gmail.com>
2024-02-28 23:20:26 +0800

    20240228.232019

--- app/getSizeOfImage.js
+++ app/getSizeOfImage.js
@@ -1,11 +1,6 @@
 const request = require('request');
 const sizeOf = require('image-size');
 
-const { promisify } = require('util');
-const fs = require('fs');
-const { resolve } = require('path');
-const { createCanvas, loadImage } = require('canvas');
-
 module.exports = function(imageUrl) {
   if (imageUrl.startsWith('data:image/')) {
     return getImageDimensions(imageUrl)
@@ -33,42 +28,19 @@ module.exports = function(imageUrl) {
   })
 }
 
+function getImageDimensions(base64Image) {
+  // Extract the base64 data from the string (remove the data URL part)
+  const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, '');
 
-// Function to get the dimensions of a base64 encoded image
-async function getImageDimensionsFromBase64(base64String) {
-  // Remove header from base64 string
-  const base64Data = base64String.replace(/^data:image\/\w+;base64,/, '');
-  // Decode base64 data
-  const buffer = Buffer.from(base64Data, 'base64');
+  // Convert the base64 string to a Buffer
+  const imageBuffer = Buffer.from(base64Data, 'base64');
 
-  // Load image from buffer
-  const img = new Promise((resolve, reject) => {
-      const image = new Image();
-      image.onload = () => resolve(image);
-      image.onerror = (err) => reject(err);
-      image.src = 'data:image/png;base64,' + base64Data;
-  });
+  // Get the dimensions of the image
+  const dimensions = sizeOf(imageBuffer);
 
-  try {
-      // Wait for image to load
-      const image = await img;
-
-      // Get dimensions
-      const { width, height } = image;
-      return { width, height };
-  } catch (error) {
-      console.error('Error reading image:', error);
-      return null;
-  }
-}
-
-// Function to get image dimensions from base64 string
-async function getImageDimensions(base64Image) {
-  try {
-      const dimensions = await getImageDimensionsFromBase64(base64Image);
-      return dimensions;
-  } catch (error) {
-      console.error('Error getting image dimensions:', error);
-      return null;
-  }
+  // Return dimensions as a JSON object
+  return {
+      width: dimensions.width,
+      height: dimensions.height
+  };
 }
